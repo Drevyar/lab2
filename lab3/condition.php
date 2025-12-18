0e155ac29ada169332f509762b2611642d7f89a2
Drevyaer <kraivich.sa@ku.th>
2025-12-18 12:32:57 +0700

    change

--- lab3/condition.php
+++ lab3/condition.php
@@ -1,11 +1,23 @@
 <?php
-$age = 18;
-
-echo "if age >= 20 display you are adult <br>";
-
-if ($age >= 20) {
-    echo "you are adult.";
-} else {
-    echo "you are not an adult.";
+$day = "Wednesday";
+echo "to day is Wednesday <br>";
+switch ($day) {
+    case "Monday":
+        echo "It's first day of week";
+        break;
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+        echo "It's normal day, do your work";
+        break;
+    case "Friday":
+        echo "วันศุกร์ - เย้! สุดสัปดาห์มาแล้ว";
+        break;
+    case "Saturday":
+    case "Sunday":
+        echo "It's free day enjoy your day";
+        break;
+    default:
+        echo "Invalid day";
 }
 ?>
